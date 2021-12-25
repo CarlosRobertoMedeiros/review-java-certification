83cb09efb3853cf24b8bac8e4eb1bfcaf5cb6191
Carlos Roberto <carlosmedeiroslima@gmail.com>
2021-12-25 12:36:14 -0300

    Atualização de revisão

--- src/main/java/classe/DataTeste.java
+++ src/main/java/classe/DataTeste.java
@@ -5,17 +5,17 @@ public class DataTeste {
 	public static void main(String[] args) {
 		
 		Data d1 = new Data();
-		d1.dia = 10;
-		d1.mes = 12;
-		d1.ano = 2021;
 		
-		var d2 = new Data(); //Inferência
-		d2.dia = 05;
-		d2.mes = 07;
-		d2.ano = 2018;
 		
-		System.out.printf(" %d/%d/%d\n", d1.dia, d1.mes, d1.ano);
-		System.out.printf(" %d/%d/%d", d2.dia, d2.mes, d2.ano);
+		var d2 = new Data(31,12,2020); //Inferência
+		
+		
+		System.out.printf(d1.obterDataFormatada());
+		System.out.printf(d2.obterDataFormatada());
+		
+		
+		//String dataFormatada1 = d1.obterDataFormatada(); 
+
 		
 	}
 
--- src/main/java/classe/Produto.java
+++ src/main/java/classe/Produto.java
@@ -6,6 +6,29 @@ public class Produto {
 	double preco;
 	double desconto;
 	
+	Produto(){
+		
+	}
+	
+	Produto(String nomeInicial){
+		nome = nomeInicial;
+		
+	}
+	
+	Produto(String nomeInicial, double precoInicial){
+		nome = nomeInicial;
+		preco = precoInicial;
+		
+	}
+	
+	double precoComDesconto() {
+		return preco * (1 - desconto);
+	}
+	
+	double precoComDesconto(double descontoDoGerente) {
+		return preco * (1 - desconto + descontoDoGerente);
+	}
+	
 	
 
 }
